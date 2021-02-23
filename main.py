df3df9d038e9da2191488ab4e1f128e0acc33169
Joshua Shannon <retrodaredevil@gmail.com>
2021-02-22 22:32:51 -0600

    Commit before I remove blue adjuster

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import board
 import neopixel
 import time
 
+DIM = 0.8
+
 
 def get_rainbow(percent: float) -> Tuple:
     spot = int(percent * 6)
@@ -27,13 +29,32 @@ def main():
     pixels_list = [
         neopixel.NeoPixel(board.D18, 300),  # AKA GPIO 18
     ]
+    pixels_list[0].auto_write = False
 
     while True:
         percent = (time.time() / 6) % 1
         for pixels in pixels_list:
             for i in range(len(pixels)):
-                pixels[i] = get_rainbow((percent + i * 0.02) % 1)
-        time.sleep(0.1)
+                color = get_rainbow((percent + i * 0.02) % 1)
+                r = color[0] / 255
+                g = color[1] / 255
+                b = color[2] / 255
+
+                if i >= 240:
+                    r **= 1.8
+                    g **= 1.7
+                    b **= 0.1
+                elif i >= 200:
+                    r **= 1.4
+                    g **= 1.2
+                    b **= 0.2
+                elif i >= 150:
+                    r **= 1.2
+                    g **= 1.1
+                    b **= 0.3
+                pixels[i] = (DIM * r * 255, DIM * g * 255, DIM * b * 255)
+            pixels.show()
+        time.sleep(.1)
 
 
 if __name__ == '__main__':
