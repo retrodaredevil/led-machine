79e57c6eece872607ddc97e0094051b035f37334
Joshua Shannon <retrodaredevil@gmail.com>
2021-04-17 16:33:51 -0500

    Parsing colors do it in order correctly

--- led_machine/color_parse.py
+++ led_machine/color_parse.py
@@ -6,7 +6,9 @@ from led_machine.color import Color
 def parse_colors(text: str) -> List[Color]:
     text = text.lower()
     result: List[Color] = []
-    for word in text.split():
+    word_list = text.split()
+    for i, word in enumerate(word_list):
+        previous_word = None if i == 0 else word_list[i - 1]
         if word.startswith("#"):
             word = word[1:]
             if len(word) == 3:
@@ -19,28 +21,28 @@ def parse_colors(text: str) -> List[Color]:
                     print(f"Couldn't parse: {repr(word)}")
             else:
                 print(f"Cannot parse word: {repr(word)}")
-    if "brown" in text or ("shallow" in text and "purple" in text):
-        result.append(Color.from_bytes(165, 42, 23))
-    if "purple" in text and "deep" in text:
-        result.append(Color.from_bytes(255, 0, 70))
-    if "purple" in text:
-        result.append(Color.from_bytes(255, 0, 255))
-    if "pink" in text:
-        result.append(Color.from_bytes(255, 100, 120))
-    if "red" in text:
-        result.append(Color.from_bytes(255, 0, 0))
-    if "green" in text:
-        result.append(Color.from_bytes(0, 255, 0))
-    if "blue" in text:
-        result.append(Color.from_bytes(0, 0, 255))
-    if "orange" in text:
-        result.append(Color.from_bytes(255, 45, 0))
-    if "yellow" in text:
-        result.append(Color.from_bytes(255, 170, 0))
-    if "teal" in text or "cyan" in text:
-        result.append(Color.from_bytes(0, 255, 255))
-    if "white" in text:
-        result.append(Color.from_bytes(255, 255, 255))
+        elif "brown" in word:
+            result.append(Color.from_bytes(165, 42, 23))
+        elif "purple" in word and ("deep" in word or (previous_word is not None and "deep" in previous_word)):
+            result.append(Color.from_bytes(255, 0, 70))
+        elif "purple" in word:
+            result.append(Color.from_bytes(255, 0, 255))
+        elif "pink" in word:
+            result.append(Color.from_bytes(255, 100, 120))
+        elif "red" in word:
+            result.append(Color.from_bytes(255, 0, 0))
+        elif "green" in word:
+            result.append(Color.from_bytes(0, 255, 0))
+        elif "blue" in word:
+            result.append(Color.from_bytes(0, 0, 255))
+        elif "orange" in word:
+            result.append(Color.from_bytes(255, 45, 0))
+        elif "yellow" in word:
+            result.append(Color.from_bytes(255, 170, 0))
+        elif "teal" in word or "cyan" in word:
+            result.append(Color.from_bytes(0, 255, 255))
+        elif "white" in word:
+            result.append(Color.from_bytes(255, 255, 255))
 
     return result
 
